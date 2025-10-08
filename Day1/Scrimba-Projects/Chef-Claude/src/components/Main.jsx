02428d98ce05f9435b82f632fffa753fa2eca81f
Ayan Sunesara <154762144+AyanSunesara@users.noreply.github.com>
2025-10-08 02:31:30 -0500

    Added functionality to show data from the input as a list

--- Day1/Scrimba-Projects/Chef-Claude/src/components/Main.jsx
+++ Day1/Scrimba-Projects/Chef-Claude/src/components/Main.jsx
@@ -1,16 +1,37 @@
 import './Main.css'
+import { useState } from 'react';
 
 export default function Main(){
+    //calling use state to store ingredients array with starting value empty
+    const [ingredients, setIngredients] = useState([]);
+
+    //copying array to a new array
+    const ingredientsMap = ingredients.map(ingredient => (
+            <li key={ingredient}>{ingredient}</li>
+        )
+    )
+
+    //setting ingredients to the array using the function and using previous array and adding new element to it
+    function HandleSubmit(formData){
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prev => [...prev, newIngredient])
+    }
+
     return(
         <main>
-            <form className="addingredient-form">
+            <form action={HandleSubmit} className="addingredient-form">
                 <input  
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add Ingredient</button>
             </form>
+
+            <ul>
+                {ingredientsMap}
+            </ul>
         </main>
     )
 }
